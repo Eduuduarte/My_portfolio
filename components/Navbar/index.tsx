2f31db8277b6820dc697faade6206834f7d1052a
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-09-30 23:20:55 -0300

    finish navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,15 +3,20 @@ import {AiFillHome, AiOutlineIdcard, AiOutlineFundProjectionScreen} from 'react-
 import {FaTools, FaPhoneSquareAlt} from 'react-icons/fa';
 import {BsFileBarGraphFill} from 'react-icons/bs';
 
+import { useState } from 'react';
+
 const Navbar= () => {
+
+    const [activiteNav, setActiveNav] = useState('#');
+
     return (
         <div className={styles.container}>
-            <a><AiFillHome className={styles.icon} /></a>
-            <a><AiOutlineIdcard className={styles.icon}/></a>
-            <a><FaTools className={styles.icon}/></a>
-            <a><AiOutlineFundProjectionScreen className={styles.icon}/></a>
-            <a><BsFileBarGraphFill className={styles.icon}/></a>
-            <a><FaPhoneSquareAlt className={styles.icon}/></a>
+            <a href='#' onClick={() => setActiveNav('#')} className={activiteNav === '#' ? styles.active : ''}><AiFillHome className={styles.icon} /></a>
+            <a href='#about' onClick={() => setActiveNav('#about')} className={activiteNav === '#about' ? styles.active : ''}><AiOutlineIdcard className={styles.icon}/></a>
+            <a href='#skills' onClick={() => setActiveNav('#skills')} className={activiteNav === '#skills' ? styles.active : ''}><FaTools className={styles.icon}/></a>
+            <a href='#service' onClick={() => setActiveNav('#service')} className={activiteNav === '#service' ? styles.active : ''}><AiOutlineFundProjectionScreen className={styles.icon}/></a>
+            <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activiteNav === '#portfolio' ? styles.active : ''}><BsFileBarGraphFill className={styles.icon}/></a>
+            <a href='#contact' onClick={() => setActiveNav('#contact')} className={activiteNav === '#contact' ? styles.active : ''}><FaPhoneSquareAlt className={styles.icon}/></a>
         </div>
     )
 }
