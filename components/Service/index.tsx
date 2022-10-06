58ac5f2ab72511804514d84e1547b560be266bb9
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-10-05 21:30:44 -0300

    finishing service

--- components/Service/index.tsx
+++ components/Service/index.tsx
@@ -1,9 +1,45 @@
 import styles from './styles.module.css';
+import {MdComputer} from 'react-icons/md';
+import {BsPhone, BsCodeSquare} from 'react-icons/bs';
+import {AiOutlineApi} from 'react-icons/ai';
 
 const Service = () => {
     return (
         <div className={styles.container} id='service'>
-            Service
+            <h4>Quais são os</h4>
+            <h1>Serviços</h1>
+            <h4>que ofereço</h4>
+
+            <div className={styles.area}>
+                <article className={styles.service}>
+                    <MdComputer className={styles.icon}/>
+                    <div className={styles.serviceText}>
+                        <h3>Desenvolvimento WEB</h3>
+                        <p>Desenvolvo sistemas WEB responsivos, leadpages, dashboards, sistemas de loja virtual e delivery.</p>
+                    </div>
+                </article>
+                <article className={styles.service}>
+                    <BsPhone className={styles.icon}/>
+                    <div className={styles.serviceText}>
+                        <h3>Desenvolvimento Aplicativos</h3>
+                        <p>Desenvolvo aplicativos mobile para Android e Iphone, que são atrativos, leves e robustos.</p>
+                    </div>
+                </article>
+                <article className={styles.service}>
+                    <AiOutlineApi className={styles.icon}/>
+                    <div className={styles.serviceText}>
+                        <h3>Criação de API</h3>
+                        <p>Desenvolvo APIs com NodeJs para aplicações WEB e Mobile, utilizando banco de dados relacional e não relacional.</p>
+                    </div>
+                </article>
+                <article className={styles.service}>
+                    <BsCodeSquare className={styles.icon}/>
+                    <div className={styles.serviceText}>
+                        <h3>Outros</h3>
+                        <p>Faço freelancer para aplicações do Office365 ( PowerApps, Sharepoint, PowerBi e Power Automate)</p>
+                    </div>
+                </article>
+            </div>
         </div>
     )
 }
