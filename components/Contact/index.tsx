023e4dac67184e821a3f994dc356e774317b350f
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-10-09 13:23:39 -0300

    init bluild the contact component

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,10 +1,43 @@
 import styles from './styles.module.css';
+import {MdEmail} from 'react-icons/md';
+import {RiWhatsappFill} from 'react-icons/ri';
+import {AiFillLinkedin} from 'react-icons/ai';
 
 const Contact = () => {
     return (
         <div className={styles.container} id="contact">
-            Contact
+            <h4>Vamos conversa?</h4>
+            <h1>Contate-me</h1>
 
+            <div className={styles.areaContact}>
+                <div className={styles.areaOptions}>
+                    <article className={styles.option}>
+                        <MdEmail className={styles.icon} />
+                        <h4>E-mail</h4>
+                        <h3>eduardoievolucao@gmail.com</h3>
+                        <a href='mailto:eduardoievolucao@gmail.com'>Envie uma mensagem</a>
+                    </article>
+                    <article className={styles.option}>
+                        <RiWhatsappFill className={styles.icon} />
+                        <h4>WhatsApp</h4>
+                        <h3>+5588998056422</h3>
+                        <a href='mailto:eduardoievolucao@gmail.com'>Envie uma mensagem</a>
+                    </article>
+                    <article className={styles.option}>
+                        <AiFillLinkedin className={styles.icon} />
+                        <h4>Linkedin</h4>
+                        <h3>Eduardo Duarte</h3>
+                        <a href='https://www.linkedin.com/in/eduuduarte/'>Envie uma mensagem</a>
+                    </article>
+                </div>
+                <form action="">
+                    <input type="name" name='name' placeholder='Seu Nome'/>
+                    <input type="email" name='email' placeholder='Seu E-mail'/>
+                    <input type="name" name='subject' placeholder='Assunto'/>
+                    <textarea name="message" id="" rows={10}></textarea>
+                    <button>Enviar</button>
+                </form>
+            </div>
         </div>
     )
 }
